7dfdf138147878f386218c4af91405fc17b4cf21
progam desk <ftc16854@notredamehighschool.com>
2022-12-29 18:06:24 -0500

    Made AprilTag autonomous opmode. (Should add timeout and default if tag not detected). Lift positions need to change after spool swap.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestAprilTags.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestAprilTags.java
@@ -29,16 +29,7 @@
 
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-
-import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
-import org.openftc.apriltag.AprilTagDetection;
-import org.openftc.easyopencv.OpenCvCamera;
-import org.openftc.easyopencv.OpenCvCameraFactory;
-import org.openftc.easyopencv.OpenCvCameraRotation;
-
-import java.util.ArrayList;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 /**
  * Original FTC opmode header block
  *
@@ -60,37 +51,9 @@ import java.util.ArrayList;
  * override the ParentOpMode runOpMode() method.
  **/
 
-@TeleOp(name="Vision Scoob", group="Linear Opmode")
-@Disabled
-public class VisionParent extends MainParentOpMode {
-
-    static final double FEET_PER_METER = 3.28084;
-
-    OpenCvCamera camera;
-    AprilTagDetectionPipeline aprilTagDetectionPipeline;
-
-
-
-    //None Of This Is Properly Set Up For Our Camera DON'T TRUST IT ITS THE MOLE IN THE TEAM
-    // Lens intrinsics
-    // UNITS ARE PIXELS
-    // NOTE: this calibration is for the C920 webcam at 800x448.
-    // You will need to do your own calibration for other configurations!
-    double fx = 578.272;
-    double fy = 578.272;
-    double cx = 402.145;
-    double cy = 221.506;
-
-    // UNITS ARE METERS
-    double tagsize = 0.166;
-
-    int numFramesWithoutDetection = 0;
-
-    final float DECIMATION_HIGH = 3;
-    final float DECIMATION_LOW = 2;
-    final float THRESHOLD_HIGH_DECIMATION_RANGE_METERS = 1.0f;
-    final int THRESHOLD_NUM_FRAMES_NO_DETECTION_BEFORE_LOW_DECIMATION = 4;
-
+@Autonomous(name="TestAprilTags", group="Linear Opmode")
+//@Disabled
+public class TestAprilTags extends VisionParent {
     @Override
     public void runOpMode() {
 
@@ -107,7 +70,7 @@ public class VisionParent extends MainParentOpMode {
 
         // run until the end of the match (driver presses STOP)
         while (opModeIsActive()) {
-
+            ApriltagId();
 
             checkEmergencyStop();
 
@@ -115,45 +78,5 @@ public class VisionParent extends MainParentOpMode {
         }
     }
 
-    public void VisionSetUp(){
-        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
-        camera = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "Webcam 1"), cameraMonitorViewId);
-        aprilTagDetectionPipeline = new AprilTagDetectionPipeline(tagsize, fx, fy, cx, cy);
-
-        camera.setPipeline(aprilTagDetectionPipeline);
-        camera.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener()
-        {
-            @Override
-            public void onOpened()
-            {
-                camera.startStreaming(800,448, OpenCvCameraRotation.UPRIGHT);
-            }
-
-            @Override
-            public void onError(int errorCode)
-            {
-
-            }
-        });
-    }
-
-    public int ApriltagId(){
-        //sleep(1000);
-        ArrayList<AprilTagDetection> aprilTags = aprilTagDetectionPipeline.getLatestDetections();
-
-        while(aprilTags.size()<1){
-            aprilTags = aprilTagDetectionPipeline.getLatestDetections();
-        }
-
-        if (aprilTags.size()>0){
-            AprilTagDetection detectedTag = aprilTags.get(0);
-            telemetry.addData("saw tag ",detectedTag.id);
-            return detectedTag.id;
-        }
-        else{
-            telemetry.addData("Dafulted to 2 0","Didn't see any tag" );
-            return 2;
-        }
-    }
 
 }
